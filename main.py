0d2ee6966d2bef3174d18a64245011aa530d1672
MarkStevenNavarro <markstevennavarro@inspladelestany.cat>
2022-09-16 17:27:16 +0200

    Afegim atribut 'qualificacions' a la classe Unitat Formativa

--- main.py
+++ main.py
@@ -15,3 +15,25 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Definició de classes
+class UnitatFormativa:
+    nom = None
+    qualificacions = None
+
+    def __init__(self, nom):
+        self.nom = nom
+
+
+# Inici del programa
+uf1 = UnitatFormativa("UF1. Desenvolupament del programari")
+uf2 = UnitatFormativa("UF2. Optimització del programari")
+uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes")
+
+uf1.qualificacions = 8
+uf2.qualificacions = 10
+uf3.qualificacions = 4
+
+print(uf1.nom, ":", uf1.qualificacions)
+print(uf2.nom, ":", uf1.qualificacions)
+print(uf3.nom, ":", uf1.qualificacions)
